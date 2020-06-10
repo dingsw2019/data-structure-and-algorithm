b5139ef306699979702fe66df9cdc0a3e6dd8e60
liupengyu <26631931@qq.com>
2020-06-10 20:18:32 +0800

    看链表代码

--- chapter3/single_list.c
+++ chapter3/single_list.c
@@ -1,3 +1,97 @@
 /**
  * 单向链表
  */
+
+#include <stdio.h>
+#include "single_list.h"
+
+bool isLast(Position P,List L) {
+    return (P->next == NULL) ? true : false;
+}
+
+bool isEmpty(List L) {
+
+}
+
+
+void makeEmpty(List L) {
+
+}
+
+void deleteList(List L) {
+
+}
+
+Position find(ElementType X, List L) {
+
+}
+Position findPrevious(ElementType X, List L) {
+
+}
+
+// 添加节点
+void insert(ElementType X, List L, Position P){
+    Position node = malloc(sizeof(struct Node));
+    if (node == NULL) {
+        perror("insert");
+        return ;
+    }
+
+    node->element = X;
+    node->next = P->next;
+    
+    P->next = node;
+}
+
+void delete(ElementType X, List L){
+
+    Position P = findPrevious(X,L);
+    Position tmpCell;
+
+    if (!isLast(P,L)) {
+        tmpCell = P->next;
+        P->next = tmpCell->next;
+        free(tmpCell);
+    }
+}
+
+void printList(List L){
+    if (isEmpty(L)) return;
+
+    Position P = L->next;
+    while (P != NULL || P->next != NULL) {
+        printf("%d -> ",P->element);
+        P = P->next;
+    }
+
+    if (P != NULL && P->next == NULL) {
+        printf("%d\n", P->element);
+    }
+}
+
+List fromArray(ElementType *A, size_t N){
+    List L = malloc(sizeof(struct Node));
+    Position P = L;
+    int i;
+    for (i=0; i<N; ++i) {
+        insert(A[i],L,P);
+        P = P->next;
+    }
+    return L;
+}
+
+Position header(List L){
+    return L;
+}
+
+Position first(List L){
+    return L->next;
+}
+
+Position advance(Position P){
+    return P->next;
+}
+
+List intersect(List L1, List L2){}
+List Union(List L1, List L2){}
+List reverseList(List L){}
---
+++ chapter3/single_list.h
@@ -0,0 +1,30 @@
+#include <stdbool.h>
+#include <stddef.h>
+
+typedef struct Node* PtrToNode;
+typedef PtrToNode List;
+typedef PtrToNode Position;
+typedef int ElementType;
+
+struct Node {
+    ElementType element;
+    Position next;
+};
+
+bool isLast(Position P,List L);
+bool isEmpty(List L);
+void makeEmpty(List L);
+void deleteList(List L);
+Position find(ElementType X, List L);
+Position findPrevious(ElementType X, List L);
+void insert(ElementType X, List L, Position P);
+void delete(ElementType X, List L);
+void printList(List L);
+List fromArray(ElementType *A, size_t N);
+Position header(List L);
+Position first(List L);
+Position advance(Position P);
+
+List intersect(List L1, List L2);
+List Union(List L1, List L2);
+List reverseList(List L);
