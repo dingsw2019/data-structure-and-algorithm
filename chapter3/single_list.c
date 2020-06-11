b3eb6bc25ac8181ecbdb77b6dec21d17d1c345a7
liupengyu <26631931@qq.com>
2020-06-11 21:48:00 +0800

    链表

--- chapter3/single_list.c
+++ chapter3/single_list.c
@@ -10,37 +10,55 @@ bool isLast(Position P,List L) {
 }
 
 bool isEmpty(List L) {
-
+    return (L->next == NULL) ? true : false;
 }
 
-
+// 清空链表
 void makeEmpty(List L) {
+    Position P = L->next;
+    Position tmp;
 
+    while(P != NULL) {
+        tmp = P->next;
+        free(P);
+        P = tmp;
+    }
 }
 
+// 释放链表
 void deleteList(List L) {
-
+    makeEmpty(L);
+    free(L);
 }
 
+// 查找节点
 Position find(ElementType X, List L) {
-
+    Position P = L->next;
+    while (P != NULL && P->element != X)
+        P = P->next;
+    return P;
 }
-Position findPrevious(ElementType X, List L) {
 
+// 查找节点, 返回目标节点的前置节点
+Position findPrevious(ElementType X, List L) {
+    Position P = L;
+    while (P->next != NULL && P->next->element != X)
+        P = P->next;
+    return P;
 }
 
 // 添加节点
 void insert(ElementType X, List L, Position P){
-    Position node = malloc(sizeof(struct Node));
-    if (node == NULL) {
+    Position tmpCell = malloc(sizeof(struct Node));
+    if (tmpCell == NULL) {
         perror("insert");
         return ;
     }
 
-    node->element = X;
-    node->next = P->next;
+    tmpCell->element = X;
+    tmpCell->next = P->next;
     
-    P->next = node;
+    P->next = tmpCell;
 }
 
 void delete(ElementType X, List L){
@@ -92,6 +110,27 @@ Position advance(Position P){
     return P->next;
 }
 
-List intersect(List L1, List L2){}
+// 交集
+List intersect(List L1, List L2) {
+    List L = malloc(sizeof(struct Node));
+    Position P = L;
+
+    Position l1 = L1->next;
+    Position l2 = L2->next;
+
+    while (l1 != NULL && l2 != NULL) {
+        if (l1->element == l2->element) {
+
+        } else if (l1->element < l2->element) {
+
+        } else {
+            // l1 = 
+        }
+    }
+}
+
+// 并集
 List Union(List L1, List L2){}
+
+
 List reverseList(List L){}
